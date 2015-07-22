a0f14c81f9601ca8072d667919b6bc5c0e13fa24
Eric Drewniak & JC <edrewniak@mitre.org>
2015-07-22 15:18:15 -0400

    Add new Project Code Breaker to Rip Repo

---
+++ CodeBreaker/src/CodeBeakerGui.java
@@ -0,0 +1,194 @@
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import javax.swing.UIManager;
+
+import java.awt.Label;
+import java.awt.Panel;
+
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class CodeBeakerGui {
+
+	private JFrame fame;
+	
+	private JTabbedPane tabbedPane;
+	private Panel EncryptionPanel;
+	private Panel DecryptionPanel;
+	
+	private JLabel lblCodeBreaker;
+	private JLabel lblKeyWord;
+	private JLabel lblScrambledMessage;
+	private Label lblMessage;
+	private Label lblEncryptedMessage;
+	private Label lblDecryptedMessage;
+	private Label lblKeyWordIs;
+	
+	private JTextField KeyWordTextField;
+	private JTextField MessageTextField;
+	private JTextField EncryptedMessageTextField;
+	private JTextField EncryptedTextField;
+	private JTextField DecryptMessageTextField;
+	private JTextField KeyWordIsTextField;
+	
+	private JButton EncryptButton;
+	private JButton DecryptButton;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					CodeBeakerGui window = new CodeBeakerGui();
+					window.fame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CodeBeakerGui() {
+		initialize();
+		EncryptionPanel();
+		DecryptionPanel();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		fame = new JFrame();
+		fame.setTitle("Code Breaker");
+		fame.setResizable(false);
+		fame.setBounds(100, 100, 532, 236);
+		fame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fame.getContentPane().setLayout(null);
+		
+		// Title Label
+		lblCodeBreaker = new JLabel("Code Breaker");
+		lblCodeBreaker.setBounds(230, 0, 71, 23);
+		fame.getContentPane().add(lblCodeBreaker);
+		
+		// Main Tab Pane
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 21, 523, 183);
+		fame.getContentPane().add(tabbedPane);
+		
+	}// End of initialize Method
+	
+	private void EncryptionPanel(){
+		
+		//////////////////////////////////////////////////////////////////////////////////////////////ENCRYPTION PANEL ////////////////////////////////////////////////////////////////////////////////
+		
+		//START OF ENCRYPTION PANEL
+		EncryptionPanel = new Panel();
+		tabbedPane.addTab("Encryption", null, EncryptionPanel, null);
+		EncryptionPanel.setLayout(null);
+		
+		// Key Word Label
+		lblKeyWord = new JLabel("Key Word:");
+		lblKeyWord.setBounds(10, 11, 51, 32);
+		EncryptionPanel.add(lblKeyWord);
+		
+		// Key Word Text Field
+		KeyWordTextField = new JTextField();
+		KeyWordTextField.setBounds(65, 11, 342, 22);
+		EncryptionPanel.add(KeyWordTextField);
+		
+		// Encrypt Button
+		EncryptButton = new JButton("Encrypt");
+		EncryptButton.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+		System.out.println("Encrypt Button pressed");
+		}
+		});
+		EncryptButton.setActionCommand("");
+		EncryptButton.setBounds(427, 11, 70, 22);
+		EncryptionPanel.add(EncryptButton);
+		
+		// Message Label
+		lblMessage = new Label("Message:");
+		lblMessage.setBounds(10, 39, 51, 22);
+		EncryptionPanel.add(lblMessage);
+		
+		// Message Text Field 
+		MessageTextField = new JTextField();
+		MessageTextField.setBounds(67, 39, 441, 22);
+		EncryptionPanel.add(MessageTextField);
+		
+		// Encrypted Message Label
+		lblScrambledMessage = new JLabel("Encrypt Message:");
+		lblScrambledMessage.setBounds(195, 67, 107, 14);
+		EncryptionPanel.add(lblScrambledMessage);
+		
+		// Encrypted Message Text Field
+		EncryptedMessageTextField = new JTextField();
+		EncryptedMessageTextField.setBounds(10, 87, 498, 58);
+		EncryptionPanel.add(EncryptedMessageTextField);
+		
+	}// End of Encryption Panel Method
+	
+	private void DecryptionPanel(){
+		///////////////////////////////////////////////////////////////////////////////////////////// DECRYPTION PANEL ///////////////////////////////////////////////////////////////////////////////
+		
+		// START OF DECRYPTION PANEL
+		DecryptionPanel = new Panel();
+		tabbedPane.addTab("Decryption", null, DecryptionPanel, null);
+		DecryptionPanel.setLayout(null);
+		
+		// Encrypted Message Label
+		lblEncryptedMessage = new Label("Encrypted Message:");
+		lblEncryptedMessage.setBounds(10, 10, 106, 22);
+		DecryptionPanel.add(lblEncryptedMessage);
+		
+		// Encrypted Text Field
+		EncryptedTextField = new JTextField();
+		EncryptedTextField.setBounds(122, 10, 313, 22);
+		DecryptionPanel.add(EncryptedTextField);
+		
+		// Decrypt Button
+		DecryptButton = new JButton("Decrypt");
+		DecryptButton.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent arg0) {
+		System.out.println("Decrypt Button pressed");
+		}
+		});
+		DecryptButton.setBounds(438, 10, 70, 22);
+		DecryptionPanel.add(DecryptButton);
+		
+		// Decrypt Message Label
+		lblDecryptedMessage = new Label("Decrypt Message");
+		lblDecryptedMessage.setBounds(212, 29, 90, 22);
+		DecryptionPanel.add(lblDecryptedMessage);
+		
+		// Decrypt Message Text Field
+		DecryptMessageTextField = new JTextField();
+		DecryptMessageTextField.setBounds(10, 57, 498, 63);
+		DecryptionPanel.add(DecryptMessageTextField);
+		
+		// KEY WORD LABAEL
+		lblKeyWordIs = new Label("The Key Word is:");
+		lblKeyWordIs.setBounds(10, 123, 90, 22);
+		DecryptionPanel.add(lblKeyWordIs);
+		
+		// KEY WORD TEXTFIELD
+		KeyWordIsTextField = new JTextField();
+		KeyWordIsTextField.setBounds(106, 126, 402, 22);
+		DecryptionPanel.add(KeyWordIsTextField);
+		
+	}//End of Decryption Panel Method
+	
+}// End of Class
