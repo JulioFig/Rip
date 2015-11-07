f5c7685eeadd56fd1174b2b43da8c4fe073a4c82
JulioFig <juliofigueroa@outlook.com>
2015-11-07 07:52:35 -0500

    Iso App Photo Filter code nearly working

--- PhotoFilter/PhotoFilter/ViewController.swift
+++ PhotoFilter/PhotoFilter/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var PhotoImageView: UIImageView!
+    
+    // Create a place to render the filtered image
+    let context = CIContext(options: nil)
 
+    @IBAction func ApplyFilter(sender: AnyObject) {
+        
+        //Create an image to filter
+        let inputImage = CIImage(image: PhotoImageView.image )
+        
+        //Create a random color to pass to a filter
+        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100) ]
+        
+        //Apply a Filter to the image
+        let filteredImage = inputImage.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+    
+        //Render the filter to the image
+        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent())
+        
+        //Render the filter change back in the interface
+        photoImageView.image = UIImage(CGImage: renderedImage)
+        
+        
+    }// End of action button for Apply Filter
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
