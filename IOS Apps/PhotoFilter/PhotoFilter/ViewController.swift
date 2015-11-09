73cebd0ed5e62b01f477867ac5cdbd9293f571a7
JulioFig <juliofigueroa@outlook.com>
2015-11-08 21:45:04 -0500

    Photo Filter app complimes in Sim: Bugs the app does not show filter button or filter capabilties

--- IOS Apps/PhotoFilter/PhotoFilter/ViewController.swift
+++ IOS Apps/PhotoFilter/PhotoFilter/ViewController.swift
@@ -9,41 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var PhotoImageView: UIImageView!
+    
+    @IBOutlet weak var photoImageView: UIImageView!
     
     // Create a place to render the filtered image
     let context = CIContext(options: nil)
-
-    @IBAction func ApplyFilter(sender: AnyObject) {
-        
-        //Create an image to filter
-        let inputImage = CIImage(image: PhotoImageView.image )
-        
-        //Create a random color to pass to a filter
-        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100) ]
-        
-        //Apply a Filter to the image
-        let filteredImage = inputImage.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
     
-        //Render the filter to the image
-        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent())
+    @IBAction func applyFilter(sender: AnyObject) {
         
-        //Render the filter change back in the interface
+        // Create an image to filter
+        let inputImage = CIImage(image: photoImageView.image!)
+        
+        // Create a random color to pass to a filter
+        let randomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)) / 100)]
+        
+        // Apply a filter to the image
+        let filteredImage = inputImage!.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+        
+        // Render the filtered image
+        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent)
+        
+        // Reflect the change back in the interface
         photoImageView.image = UIImage(CGImage: renderedImage)
         
-        
-    }// End of action button for Apply Filter
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
